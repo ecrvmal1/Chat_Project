662b61d3c277ef3200df7e96e9dc14f0d382b8ab
Vladimir <ecrvmal@yandex.ru>
2023-05-27 12:46:48 -0700

    Lesson3 TCP closing

--- Lesson3/client.py
+++ Lesson3/client.py
@@ -24,52 +24,52 @@ if __name__ == '__main__':
 
     print(f'port : {client_port} \n'
           f'IP Address : {client_ip_address}\n')
-    if not connection:
-        message = "please connect to server"
+    message = ""
     while True:
-        action = terminal_info(connection, message)
+        action = terminal_info(message)
         if action == 1:
-            if connection is True:
-                message = "Server is already connected"
-            else:
-                connection, transport = client_connection(client_ip_address, client_port)
-                message = "Server connected"
-                msg = presence_msg()
-                send_message(transport, msg)
-                try:
-                    message = process_ans(get_message(transport))
-                except (ValueError, json.JSONDecodeError):
-                    message = 'Not able to decode from SERVER'
+            connection, transport = client_connection(client_ip_address, client_port)
+            msg = presence_msg()
+            send_message(transport, msg)
+            try:
+                message = process_ans(get_message(transport))
+                client_disconnection(transport)
+            except (ValueError, json.JSONDecodeError):
+                message = 'Not able to decode from SERVER'
 
         elif action == 2:
-            if connection is True:
-                msg = user_msg()
-                # msg = presence_msg()
-                # print(f'user_msg = {msg}')
-                send_message(transport, msg)
-                try:
-                    message = process_ans(get_message(transport))
-                except (ValueError, json.JSONDecodeError):
-                    message = 'Not able to decode from SERVER'
-            else:
-                message = "Please connect to server"
+            connection, transport = client_connection(client_ip_address, client_port)
+            msg = user_msg()
+            send_message(transport, msg)
+            try:
+                message = process_ans(get_message(transport))
+                client_disconnection(transport)
+            except (ValueError, json.JSONDecodeError):
+                message = 'Not able to decode from SERVER'
 
         elif action == 3:
             if connection is True:
+                connection, transport = client_connection(client_ip_address, client_port)
                 msg = {'action': 'quit'}
                 send_message(transport, msg)
-                connection = client_disconnection(transport)
-                print('***********************************')
-                print('        Server disconnected        ')
-                print('***********************************')
-                time.sleep(2)
-                sys.exit(0)
+                try:
+                    message = process_ans(get_message(transport))
+                    client_disconnection(transport)
+                except (ValueError, json.JSONDecodeError):
+                    message = 'Not able to decode from SERVER'
+
         elif action == 4:
             pass
         elif action == 5:
             pass
+        elif action == 6:
+            print('******************************')
+            print('        program closing       ')
+            print('******************************')
+            time.sleep(2)
+            sys.exit(0)
         else:
-            raise ValueError
+            message = " Please enter menu item 1...6"
 
 
 
--- Lesson3/launcher.py
+++ Lesson3/launcher.py
@@ -6,16 +6,21 @@ while True:
     ANSWER = input('Choose action:\n'
                    'q- quit \n'
                    's- run server\n'
+                   'c- run client\n'
                    'x - close all windows')
     if ANSWER == 'q':
         break
     elif ANSWER == 's':
+        # PROCESS.append(subprocess.Popen('python server.py',
+        #                                 creationflags=subprocess.CREATE_NEW_CONSOLE))
         PROCESS.append(subprocess.Popen('python server.py',
-                                        creationflags=subprocess.CREATE_NEW_CONSOLE))
-
-        for i in range(5):
-            PROCESS.append(subprocess.Popen('python client.py',
-                                            creationflags=subprocess.CREATE_NEW_CONSOLE))
+                                        shell=True))
+    elif ANSWER == 'c':
+        # for i in range(5):
+            # PROCESS.append(subprocess.Popen('python client.py',
+            #                                 creationflags=subprocess.CREATE_NEW_CONSOLE))    for windows
+        PROCESS.append(subprocess.Popen('python client.py',
+                                        shell=True))
 
     elif ANSWER == 'x':
         while PROCESS:
