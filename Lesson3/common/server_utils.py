244089a2116a5452f381e37e2d049500349a7c9e
Vladimir <ecrvmal@yandex.ru>
2023-06-08 03:23:13 -0700

    Sprint7 Completed

--- Lesson3/common/server_utils.py
+++ Lesson3/common/server_utils.py
@@ -1,122 +1,119 @@
-import sys
-import re
-import os
-import time
+import argparse
 import json
 import logging
-from socket import *
-from deco_log import log_debug
+import sys
 
-from common.server_variables import ENCODING, RESPONSE, ERROR, MAX_PACKAGE_LENGTH, USER, \
-    ACTION, PRESENCE, TIME, ACCOUNT_NAME
+from Lesson3.common.errors import NonDictInputError, IncorrectDataRecivedError, JSONDecodeError
 
+sys.path.append('../')
+
+from common.server_variables import ACTION, PRESENCE, TIME, ACCOUNT_NAME, USER, MAX_PACKAGE_LENGTH, \
+RESPONSE, MESSAGE, MESSAGE_TEXT, ERROR, DEFAULT_SERVER_IP_ADDRESS, DEFAULT_SERVER_PORT, ENCODING
+from deco_log import log
+
+# Инициализация логирования сервера.
 LOGGER = logging.getLogger('server_logger')
 
 
-@log_debug
-def get_ip_address(args_list) -> str:
-    ip_addr = None
-    if '-a' in args_list:
-        try:
-            ip_addr = str(args_list[args_list.index('-a') + 1])
-        except IndexError:
-            print('after "-a", Server IP address must be indicated')
-            sys.exit(1)
-        reg_exp = re.compile(r'([0-9]{1,3}[\.]){3}[0-9]{1,3}')
-        if reg_exp.match(ip_addr):
-            pass
-        else:
-            print(' IP address value must be in format "xxx.xxx.xxx.xxx" ')
-            sys.exit(1)
-    return ip_addr
-
-
-@log_debug
-def get_port(args_list) -> int:
-    port_number = None
-    if '-p' in args_list:
-        try:
-            port_number = int(sys.argv[sys.argv.index('-p') + 1])
-        except IndexError:
-            print('after "-p", server port value must be indicated')
-            sys.exit(1)
-        if port_number < 1024 or port_number > 65535:
-            print(' Server port number must be in interval 1024...65535')
-            exit(1)
-    return port_number
-
-
-@log_debug
-def process_incoming_message(message):
+@log
+def process_client_message(message, messages_list, client):
     """
-    Обработчик сообщений от клиентов, принимает словарь -
-    сообщение от клинта, проверяет корректность,
-    возвращает словарь-ответ для клиента
-
+    Обработчик сообщений от клиентов, принимает словарь - сообщение от клинта,
+    проверяет корректность, отправляет словарь-ответ для клиента с результатом приёма.
     :param message:
+    :param messages_list:
+    :param client:
     :return:
     """
+    LOGGER.debug(f'Разбор сообщения от клиента : {message}')
+    print(f'processing message {message} from {client}')
+    # Если это сообщение о присутствии, принимаем и отвечаем, если успех
     if ACTION in message \
             and message[ACTION] == PRESENCE \
             and TIME in message \
-            and 'user' in message \
-            and message['user']['account_name'] == 'Guest':
-        # print('RESPONSE: 200')
-        LOGGER.info('RESPONSE: 200')
-        return {'response': {RESPONSE: 200}}
-    if ACTION in message \
-            and message[ACTION] == 'msg' \
+            and USER in message \
+            and message[USER][ACCOUNT_NAME] == 'Guest':
+        send_message(client, {RESPONSE: 200})
+        return
+    # Если это сообщение, то добавляем его в очередь сообщений. Ответ не требуется.
+    elif ACTION in message \
+            and message[ACTION] == MESSAGE \
             and TIME in message \
-            and 'to' in message \
-            and message['from'] == 'Guest':
-        # print('RESPONSE: 201')
-        LOGGER.info('RESPONSE: 201')
-        return {'response': {RESPONSE: 201}}
-    if ACTION in message \
-            and message[ACTION] == 'quit':
-        # print('quit , RESPONSE: 202')
-        LOGGER.info('RESPONSE: 202')
-        return {'response': {RESPONSE: 202},
-                'quit': " "}
-    print('RESPONSE: 400, error: Bad Request')
-    LOGGER.error('RESPONSE: 400, error: Bad Request')
-    return {'response': {
-        'response': 400,
-        'error': 'Bad Request'
-    }}
+            and MESSAGE_TEXT in message:
+        messages_list.append((message[ACCOUNT_NAME], message[MESSAGE_TEXT]))
+        print (f'message added to message_list {messages_list}')
+        return
+    # Иначе отдаём Bad request
+    else:
+        send_message(client, {
+            RESPONSE: 400,
+            ERROR: 'Bad Request'
+        })
+        print(f'sent error message to {client}')
+        return
 
 
-@log_debug
+@log
+def arg_parser():
+    """Парсер аргументов коммандной строки"""
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-p', default=DEFAULT_SERVER_PORT, type=int, nargs='?')
+    parser.add_argument('-a', default=DEFAULT_SERVER_IP_ADDRESS, nargs='?')
+    namespace = parser.parse_args(sys.argv[1:])
+    listen_address = namespace.a
+    listen_port = namespace.p
+
+    # проверка получения корретного номера порта для работы сервера.
+    if not 1023 < listen_port < 65536:
+        LOGGER.critical(
+            f'Попытка запуска сервера с указанием неподходящего порта '
+            f'{listen_port}. Допустимы адреса с 1024 до 65535.')
+        sys.exit(1)
+
+    return listen_address, listen_port
+
+
+@log
 def send_message(sock, message):
-    json_msg = json.dumps(message)
-    encoded_msg = json_msg.encode(ENCODING)
-    # print(f'sending message:  {encoded_msg}')
-    sock.send(encoded_msg)
-    LOGGER.info('message has been sent')
+    """
+    Утилита кодирования и отправки сообщения
+    принимает словарь и отправляет его
+    :param sock:
+    :param message:
+    :return:
+    """
+    print(f'sending message {message}  to {sock}')
+    if not isinstance(message, dict):
+        raise NonDictInputError
+    js_message = json.dumps(message)
+    encoded_message = js_message.encode(ENCODING)
+    sock.send(encoded_message)
 
 
-@log_debug
-def get_message(client) -> dict:
+
+@log
+def get_message(client):
     """
-    Утилита приёма и декодирования сообщения
-    The function receives and decodes messages from Server
-    if received unexpected responce, the function raise error
-    :param client: bytes
-    :return: dict
+    Утилита приёма и декодирования сообщения принимает байты выдаёт словарь,
+    если приняточто-то другое отдаёт ошибку значения
+    :param client:
+    :return:
     """
-    encoded_response = client.recv(MAX_PACKAGE_LENGTH)
-    # print(f'encoded response : {encoded_response}')
+    try:
+        encoded_response = client.recv(MAX_PACKAGE_LENGTH)
+    except ConnectionResetError:
+        raise ConnectionResetError
     if isinstance(encoded_response, bytes):
-        json_response = encoded_response.decode(ENCODING)
-        response = json.loads(json_response)
+        try:
+            json_response = encoded_response.decode(ENCODING)
+            response = json.loads(json_response)
+        except JSONDecodeError:
+            raise IncorrectDataRecivedError
         if isinstance(response, dict):
-            LOGGER.info('got message from client')
+            print(f'got message {response} from {client}')
             return response
-    LOGGER.error('got message from client with incorrect encoding')
-    raise ValueError
-
-
-
+        raise IncorrectDataRecivedError
+    raise IncorrectDataRecivedError
 
 
 
