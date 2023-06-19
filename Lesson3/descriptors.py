8899b88e54d369438285aaa62da274c46c6be1d1
Vladimir <ecrvmal@yandex.ru>
2023-06-19 08:29:44 -0700

    Sprint10 Initial commit

---
+++ Lesson3/descriptors.py
@@ -0,0 +1,14 @@
+import logging
+logger = logging.getLogger('server')
+
+
+class Port:
+    def __set__(self, instance, value):
+        if not 1023 < value < 65536:
+            logger.criticalc(f'incorrect port {value} number, program exit')
+            print(f'incorrect port {value} number, program exit')
+            exit(1)
+        instance.__dict__[self.name] = value
+
+    def __set_name__(self, owner, name):
+        self.name = name
